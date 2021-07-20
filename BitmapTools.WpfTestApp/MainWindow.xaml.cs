52a6d3660b17c463d89c01bf0d02a7696db8504c
MRGRD56 <Bukinist56@mail.ru>
2021-07-20 16:45:08 +0500

    v1.1.0 add extension methods for resizing and compressing images

--- BitmapTools.WpfTestApp/MainWindow.xaml.cs
+++ BitmapTools.WpfTestApp/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Windows;
 using BitmapTools.Wpf;
+using Size = System.Drawing.Size;
 
 namespace BitmapTools.WpfTestApp
 {
@@ -18,11 +19,13 @@ namespace BitmapTools.WpfTestApp
             InitializeComponent();
 
             var bitmap = new Bitmap(150, 100);
-            var graphics = Graphics.FromImage(bitmap);
-            graphics.Clear(ColorFactory.CreateColorFromHex("0x1e88e5"));
-            var textFont = new Font(DefaultFontFamily, 16, System.Drawing.FontStyle.Bold);
-            var fgBrush = new SolidBrush(ColorFactory.CreateColorFromArgb(0xffffeb3b));
-            graphics.DrawString(DateTime.Now.ToShortDateString(), textFont, fgBrush, new PointF(0, 0));
+            using (var bitmapGraphics = Graphics.FromImage(bitmap))
+            {
+                bitmapGraphics.Clear(ColorFactory.CreateColorFromHex("0x1e88e5"));
+                var textFont = new Font(DefaultFontFamily, 16, System.Drawing.FontStyle.Bold);
+                var fgBrush = new SolidBrush(ColorFactory.CreateColorFromArgb(0xffffeb3b));
+                bitmapGraphics.DrawString(DateTime.Now.ToShortDateString(), textFont, fgBrush, new PointF(0, 0));
+            }
 
             var bitmapSource = bitmap.ToBitmapSource();
             MainImage.Source = bitmapSource;
@@ -30,6 +33,32 @@ namespace BitmapTools.WpfTestApp
             bitmap.Save("original.png", ImageFormat.Png);
             convertedBitmap.Save("converted.png", ImageFormat.Png);
             bitmap.Resize(75, 50).Save("resized.png", ImageFormat.Png);
+            bitmap.SaveCompressed("compressed1.jpg", 10);
+            bitmap.Compress(10).Save("compressed2.jpg", ImageFormat.Jpeg);
+
+            var screenSize = new Size((int) SystemParameters.VirtualScreenWidth, (int) SystemParameters.VirtualScreenHeight);
+            var screenshot = new Bitmap(screenSize.Width, screenSize.Height);
+            using (var screenshotGraphics = Graphics.FromImage(screenshot))
+            {
+                screenshotGraphics.CopyFromScreen(0, 0, 0, 0, screenSize);
+            }
+            
+            screenshot.Save("screen.jpg", ImageFormat.Jpeg);
+            screenshot.Resize(2).Save("screen_x2.jpg", ImageFormat.Jpeg);
+            screenshot.Resize(0.5).Save("screen_x05.jpg", ImageFormat.Jpeg);
+
+            var lightWeightScreenshot1 = screenshot
+                .Resize(1280, 720);
+            
+            lightWeightScreenshot1.Save("screenshot_lw_1.jpg", ImageFormat.Jpeg);
+            
+            var lightWeightScreenshot2 = screenshot
+                .Resize(1280, 720)
+                .Compress(50);
+            
+            lightWeightScreenshot2.Save("screenshot_lw_2.jpg", ImageFormat.Jpeg);
+            
+            Close();
         }
     }
 }
--- BitmapTools/ImageExtensions.cs
+++ BitmapTools/ImageExtensions.cs
@@ -1,4 +1,4 @@
-﻿#if NET5_0_OR_GREATER || NETCOREAPP3_1 || NETSTANDARD2_1
+﻿#if NET5_0 || NET5_0_OR_GREATER || NETCOREAPP3_1 || NETSTANDARD2_1
 #define NETSTANDARD2_1_COMPATIBLE //.NET 5.0 or .NET Core 3.1 or .NET Standard 2.1
 #endif
 
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BitmapTools
@@ -17,7 +18,7 @@ namespace BitmapTools
     public static class ImageExtensions
     {
         private static readonly ImageFormat DefaultImageFormat = ImageFormat.Png;
-        
+
         /// <summary>
         /// Creates a stream from the <paramref name="image"/>. The format is PNG.
         /// </summary>
@@ -38,41 +39,40 @@ namespace BitmapTools
             image.Save(memoryStream, imageFormat);
             return memoryStream;
         }
-        
+
         /// <summary>
         /// Returns an array of bytes from the <paramref name="image"/>. The format is PNG.
         /// </summary>
         /// <param name="image"></param>
         /// <returns></returns>
         public static
-            #if NETSTANDARD2_1_COMPATIBLE
+#if NETSTANDARD2_1_COMPATIBLE
             Memory<byte>
-            #else
+#else
             byte[]
-            #endif
+#endif
             GetBytes(this Image image) =>
             image.GetBytes(DefaultImageFormat);
-        
+
         /// <summary>
         /// Returns an array of bytes from the <paramref name="image"/>.
         /// </summary>
         /// <param name="image"></param>
         /// <param name="imageFormat"></param>
         /// <returns></returns>
-        public static 
-            #if NETSTANDARD2_1_COMPATIBLE
+        public static
+#if NETSTANDARD2_1_COMPATIBLE
             Memory<byte>
-            #else
+#else
             byte[]
-            #endif
+#endif
             GetBytes(this Image image, ImageFormat imageFormat)
         {
             using var memoryStream = image.GetStream(imageFormat);
             return memoryStream.ToArray();
         }
-        
-        #if NETSTANDARD2_1_COMPATIBLE
 
+#if NETSTANDARD2_1_COMPATIBLE
         /// <summary>
         /// Returns an array of bytes from the <paramref name="image"/> in asynchronous mode. The format is PNG.
         /// </summary>
@@ -92,9 +92,9 @@ namespace BitmapTools
             await using var memoryStream = image.GetStream();
             return memoryStream.ToArray();
         }
-        
-        #endif
-        
+
+#endif
+
         /// <summary>
         /// Resizes the <paramref name="image"/> to the specified <paramref name="width"/> and <paramref name="height"/>.
         /// </summary>
@@ -103,6 +103,122 @@ namespace BitmapTools
         /// <param name="height">The height to resize to.</param>
         /// <returns>The resized image.</returns>
         public static Bitmap Resize(this Image image, int width, int height)
+        {
+            // return resizeMode switch
+            // {
+            //     ImageResizeMode.HighQuality => ResizeHQ(image, width, height),
+            //     ImageResizeMode.StandardQuality => ResizeLQ(image, width, height),
+            //     _ => throw new ArgumentOutOfRangeException(nameof(resizeMode), resizeMode, null)
+            // };
+
+            return ResizeHQ(image, width, height);
+        }
+
+        /// <summary>
+        /// Resizes the <paramref name="image"/> to the specified <paramref name="size"/>.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="size">The size to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap Resize(this Image image, Size size) =>
+            image.Resize(size.Width, size.Height);
+
+        /// <summary>
+        /// Multiplies the size of the <paramref name="image"/> by a <paramref name="sizeMultiplier"/>.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="sizeMultiplier">The value by which the size of the <paramref name="image"/> will be multiplied.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap Resize(this Image image, double sizeMultiplier)
+        {
+            if (sizeMultiplier < 0)
+            {
+                throw new ArgumentException("Size multiplier cannot be less than zero", nameof(sizeMultiplier));
+            }
+
+            var originalImageSize = image.Size;
+            var resizedImageSize = new Size(
+                (int) Math.Round(originalImageSize.Width * sizeMultiplier),
+                (int) Math.Round(originalImageSize.Height * sizeMultiplier));
+
+            return image.Resize(resizedImageSize);
+        }
+
+        /// <summary>
+        /// Compresses the <paramref name="image"/> to the specified <paramref name="quality"/> level using the JPEG format.
+        /// </summary>
+        /// <param name="image">The image to compress.</param>
+        /// <param name="quality">The compressed image quality. From 0 to 100.</param>
+        /// <returns>The compressed image.</returns>
+        public static Bitmap Compress(this Image image, long quality)
+        {
+            var compressedImageStream = new MemoryStream();
+            image.SaveCompressed(compressedImageStream, quality);
+
+            return new Bitmap(compressedImageStream);
+        }
+
+        /// <summary>
+        /// Compresses the <paramref name="image"/> to the specified <paramref name="quality"/> and saves it in JPEGs format.
+        /// </summary>
+        /// <param name="image">The image to compress and save.</param>
+        /// <param name="stream">The stream to which the image will be saved.</param>
+        /// <param name="quality">The compressed image quality. From 0 to 100.</param>
+        public static void SaveCompressed(this Image image, Stream stream, long quality)
+        {
+            image.Save(stream, GetEncoder(ImageFormat.Jpeg), GetEncoderParameters(quality));
+        }
+
+        /// <summary>
+        /// Compresses the <paramref name="image"/> to the specified <paramref name="quality"/> and saves it in JPEGs format.
+        /// </summary>
+        /// <param name="image">The image to compress and save.</param>
+        /// <param name="fileName">The name of the file where the image will be saved.</param>
+        /// <param name="quality">The compressed image quality. From 0 to 100.</param>
+        public static void SaveCompressed(this Image image, string fileName, long quality)
+        {
+            image.Save(fileName, GetEncoder(ImageFormat.Jpeg), GetEncoderParameters(quality));
+        }
+
+        /// <summary>
+        /// Compresses the <paramref name="image"/> to the specified <paramref name="quality"/> and saves it in the specified format.
+        /// </summary>
+        /// <param name="image">The image to compress and save.</param>
+        /// <param name="stream">The stream to which the image will be saved.</param>
+        /// <param name="quality">The compressed image quality. From 0 to 100.</param>
+        /// <param name="imageFormat">The image format.</param>
+        public static void SaveCompressed(this Image image, Stream stream, long quality, ImageFormat imageFormat)
+        {
+            image.Save(stream, GetEncoder(imageFormat), GetEncoderParameters(quality));
+        }
+
+        /// <summary>
+        /// Compresses the <paramref name="image"/> to the specified <paramref name="quality"/> and saves it in the specified format.
+        /// </summary>
+        /// <param name="image">The image to compress and save.</param>
+        /// <param name="fileName">The name of the file where the image will be saved.</param>
+        /// <param name="quality">The compressed image quality. From 0 to 100.</param>
+        /// <param name="imageFormat">The image format.</param>
+        public static void SaveCompressed(this Image image, string fileName, long quality, ImageFormat imageFormat)
+        {
+            image.Save(fileName, GetEncoder(imageFormat), GetEncoderParameters(quality));
+        }
+
+        /// <summary>
+        /// Changes the <paramref name="pixelFormat"/> for the <paramref name="image"/>.
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="pixelFormat"></param>
+        /// <returns></returns>
+        public static Bitmap ChangePixelFormat(this Image image, PixelFormat pixelFormat)
+        {
+            var bmp = new Bitmap(image.Width, image.Height, pixelFormat);
+            using var graphics = Graphics.FromImage(bmp);
+            graphics.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height));
+            return bmp;
+        }
+        
+        private static Bitmap ResizeHQ(Image image, int width, int height)
         {
             var destRect = new Rectangle(0, 0, width, height);
             var destImage = new Bitmap(width, height);
@@ -118,17 +234,31 @@ namespace BitmapTools
 
             using var wrapMode = new ImageAttributes();
             wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-            graphics.DrawImage(image, destRect, 0, 0, image.Width,image.Height, GraphicsUnit.Pixel, wrapMode);
+            graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
 
             return destImage;
         }
 
-        /// <summary>
-        /// Resizes the <paramref name="image"/> to the specified <paramref name="size"/>.
-        /// </summary>
-        /// <param name="image">The image to resize.</param>
-        /// <param name="size">The size to resize to.</param>
-        /// <returns>The resized image.</returns>
-        public static Bitmap Resize(this Image image, Size size) => image.Resize(size.Width, size.Height);
+        private static Bitmap ResizeLQ(Image image, int width, int height)
+        {
+            return new(image, new Size(width, height));
+        }
+
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            var codecs = ImageCodecInfo.GetImageEncoders();
+            return codecs.FirstOrDefault(codec => codec.FormatID == format.Guid);
+        }
+
+        private static EncoderParameters GetEncoderParameters(long quality)
+        {
+            return new()
+            {
+                Param = new[]
+                {
+                    new EncoderParameter(Encoder.Quality, quality)
+                }
+            };
+        }
     }
 }
